806e43123f63dde142067c62f40af56fb6197e5c
Agalo-Simon <agalosimon@outlook.com>
2026-05-17 22:01:37 +0200

    fix: update CRUD controllers and mongoose update options

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+require('node:dns/promises').setServers(['1.1.1.1', '8.8.8.8']);
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-// const itemRoutes = require("./route/itemRoutes");
+const itemRoutes = require("./route/itemRoutes");
+
 
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("Connected to MongoDB")
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+  })
 }).catch((err) => {
   console.error("Error connecting to MongoDB:", err);
 });
 
 //--- routes ---
-const itemRoutes = require("./route/itemRoutes");
 app.use("/api/items", itemRoutes)
-
--- controllers/itemControllers.js
+++ controllers/itemControllers.js
@@ -1,34 +1,46 @@
-const item = require("../models/item");
 const Item = require("../models/item");
 
 // GET all items
-getItems = async (req, res) => {
+const getItems = async (req, res) => {
   const items = await Item.find().sort({ createdAt: -1 });
   res.json(items);
 };
 
 // GET single item
-getItem = async (req, res) => {
+const getItem = async (req, res) => {
   const item = await Item.findById(req.params.id);
   res.json(item);
 };
 
 // CREATE item
-createItem = async (req, res) => {
-  const item = await Item.create(req.body);
-  res.status(201).json(item);
+const createItem = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const item = await Item.create(req.body);
+
+    res.status(201).json(item);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // UPDATE item
-updateItem = async (req, res) => {
-  const item = await Item.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.json(item);
+const updateItem = async (req, res) => {
+  try {
+    const item = await Item.findByIdAndUpdate(req.params.id, req.body, {returnDocument: "after"});
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    } 
+    res.status(200).json(item);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating item" });
+  }
 };
 
 // DELETE item
-deleteItem = async (req, res) => {
+const deleteItem = async (req, res) => {
   try {
     const item = await Item.findByIdAndDelete(req.params.id);
     if (!item) {
